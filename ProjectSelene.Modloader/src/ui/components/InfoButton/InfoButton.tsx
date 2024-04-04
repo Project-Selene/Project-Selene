8792c6c822fe9424f77e75396bcbfa53a786b848
2767mr <2767mr@gmx.at>
2024-04-04 10:29:29 +0200

    Fix info button position

--- ProjectSelene.Modloader/src/ui/components/InfoButton/InfoButton.tsx
+++ ProjectSelene.Modloader/src/ui/components/InfoButton/InfoButton.tsx
@@ -3,13 +3,12 @@ import { IconButton } from '@mui/material';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { setInfoOpen } from '../../state/state.reducer';
-import './InfoButton.module.scss';
 
 
 export function InfoButton() {
 	const dispatch = useDispatch();
 
-	return <IconButton className="infoButton text-primary" onClick={() => dispatch(setInfoOpen(true))}>
+	return <IconButton className="text-primary position-absolute bottom-0 end-0" onClick={() => dispatch(setInfoOpen(true))}>
 		<Info />
 	</IconButton >;
 }
