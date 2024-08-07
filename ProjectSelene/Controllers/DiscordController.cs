ff3dd3a2fca93c470f551205e8f748d2dd5c8701
2767mr <2767mr@gmx.at>
2024-08-07 20:39:31 +0200

    Next attempt

--- ProjectSelene/Controllers/DiscordController.cs
+++ ProjectSelene/Controllers/DiscordController.cs
@@ -20,27 +20,35 @@ public class DiscordController(ILogger<DiscordController> logger, IMapper mapper
     }
 
     [HttpPost("interactions")]
-    public async Task<InteractionsResponse> Interactions([FromBody] InteractionsRequest req)
+    public async Task<ActionResult<InteractionsResponse>> Interactions([FromBody] InteractionsRequest req)
     {
         logger.LogWarning("Discord request received: {request}", HttpContext.Request.Headers);
 
         try
         {
+            var publicKey = Convert.FromHexString("fdca6a1ca0e2a44f7b131699bfbfaba5d225b9a1740a8b1eca3e894e5b2915fc");
 
+            var timeStamp = Encoding.ASCII.GetBytes(HttpContext.Request.Headers["X-Signature-Timestamp"].ToString().Trim());
+            var signature = Convert.FromHexString(HttpContext.Request.Headers["X-Signature-Ed25519"].ToString().Trim());
+            
             using var ms = new MemoryStream();
             HttpContext.Request.Body.Position = 0;
             await HttpContext.Request.Body.CopyToAsync(ms);
 
             logger.LogWarning("Body {body}", Convert.ToBase64String(ms.ToArray()));
 
-            logger.LogWarning("Timestamp {timestamp}", HttpContext.Request.Headers["X-Signature-Timestamp"].ToString());
             var signer = SignerUtilities.GetSigner("Ed25519");
-            signer.Init(false, new Ed25519PublicKeyParameters(Convert.FromBase64String("fdca6a1ca0e2a44f7b131699bfbfaba5d225b9a1740a8b1eca3e894e5b2915fc")));
-            signer.BlockUpdate(Encoding.ASCII.GetBytes(HttpContext.Request.Headers["X-Signature-Timestamp"].ToString()));
+            signer.Init(false, new Ed25519PublicKeyParameters(publicKey));
+            signer.BlockUpdate(timeStamp);
             signer.BlockUpdate(ms.ToArray());
-            var valid = signer.VerifySignature(Convert.FromBase64String(HttpContext.Request.Headers["X-Signature-Ed25519"].ToString()));
+            var valid = signer.VerifySignature(signature);
 
             logger.LogWarning("Signature verification: {valid}", valid);
+
+            if (!valid)
+            {
+                return StatusCode(401, "invalid request signature");
+            }
         }
         catch (Exception ex)
         {
@@ -49,9 +57,9 @@ public class DiscordController(ILogger<DiscordController> logger, IMapper mapper
 
         if (req.Type == 1)
         {
-            return new() { Type = 1 };
+            return new InteractionsResponse() { Type = 1 };
         }
 
-        return new() { Type = 0 };
+        return new InteractionsResponse() { Type = 0 };
     }
 }
